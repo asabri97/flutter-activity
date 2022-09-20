5ed6ff02fe186b2dc73334f8f0364c1b1d56f257
Abrish Sabri <abrishsabri97@gmail.com>
2022-09-20 15:44:52 +0200

    domain folder initial setup

---
+++ lib/features/random_activity/domain/repositories/random_activity_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+
+import '../entities/random_activity.dart';
+
+abstract class RandomActivityRepository {
+  Future<Either<Failure, RandomActivity?>>? getRandomActivity();
+}
